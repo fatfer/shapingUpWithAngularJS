7cd6cb0cfaf274c8f0c7a94545c437947681e49f
Fer <pher.gm@gmail.com>
2014-05-15 23:48:52 +0200

    added images, customized bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-	var app = angular.module('store',[]);
+	var app = angular.module('gemStore',[]);
 
 	app.controller('StoreController', function(){
 		this.products = gems;
@@ -7,18 +7,32 @@
 
 	var gems = [
 		{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: '...',
-			canPurchase: true,
-			soldOut: false
+			name: 'Azurite',
+		    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+		    shine: 8,
+		    price: 110.50,
+		    rarity: 7,
+		    color: '#CCC',
+		    faces: 14,
+		    images: [
+		      "images/gem-02.gif",
+		      "images/gem-05.gif",
+		      "images/gem-09.gif"
+		    ]
 		},
 		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: '...',
-			canPurchase: false,
-			soldOut: false
+			name: 'Bloodstone',
+			description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+			shine: 9,
+			price: 22.90,
+			rarity: 6,
+			color: '#EEE',
+			faces: 12,
+			images: [
+				"images/gem-01.gif",
+			    "images/gem-03.gif",
+			    "images/gem-04.gif"
+			]
 		}
 	];
 
